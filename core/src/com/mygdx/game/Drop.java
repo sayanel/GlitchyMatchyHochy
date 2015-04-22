7eac9fe7c227e5ae581f9ae8e0c22b6dd54506be
sayanel <maximilien.glineur@hotmail.fr>
2015-04-22 14:44:07 +0200

    sound ok

---
+++ core/src/com/mygdx/game/Drop.java
@@ -0,0 +1,42 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+
+public class Drop implements ApplicationListener {
+    private Texture dropImage;
+    private Texture bucketImage;
+    private Sound dropSound;
+    private Music rainMusic;
+
+    @Override
+    public void create() {
+        // load the images for the droplet and the bucket, 64x64 pixels each
+        dropImage = new Texture(Gdx.files.internal("droplet.png"));
+        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+        // load the drop sound effect and the rain background "music"
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+        // start the playback of the background music immediately
+        rainMusic.setLooping(true);
+        rainMusic.play();
+    }
+
+    @Override public void render(){}
+    @Override public void dispose(){}
+    @Override public void resize(int width, int height){}
+    @Override public void pause(){}
+    @Override public void resume(){}
+
+
+
+
+
+
+}
+// rest of class omitted for clarity
