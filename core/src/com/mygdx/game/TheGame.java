6251bad5112c8632bee5dd3c94edcd607a92d248
Mehdi-Antoine <mehdi.mahfoudi@gmail.com>
2015-04-23 15:42:06 +0200

    creation branche level

--- core/src/com/mygdx/game/TheGame.java
+++ core/src/com/mygdx/game/TheGame.java
@@ -12,16 +12,11 @@ public class TheGame extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 50, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 }
