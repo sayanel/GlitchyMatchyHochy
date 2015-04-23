8255bd7a6470542c15c48791468eef6acfaa2b55
Hisuuu <hp.hisu@gmail.com>
2015-04-23 16:23:09 +0200

    init PackageScreen Branch

--- core/src/com/fatman/screen/TheGame.java
+++ core/src/com/fatman/screen/TheGame.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TheGame extends ApplicationAdapter {
-
+	//TO-DO
 	@Override
 	public void create () {
 
