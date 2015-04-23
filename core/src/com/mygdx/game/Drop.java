9791635c1ec35ac33a1769c555fd482c1cd69d57
sayanel <maximilien.glineur@hotmail.fr>
2015-04-23 16:14:30 +0200

    yo

--- core/src/com/mygdx/game/Drop.java
+++ core/src/com/mygdx/game/Drop.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.fatman.screen.MainMenuScreen;
 
 
 public class Drop extends Game {
