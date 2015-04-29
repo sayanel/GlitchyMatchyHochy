e94cd94a72afcc9051ffe9b5c70d1ff518e19ba4
Hisuuu <hp.hisu@gmail.com>
2015-04-29 11:30:24 +0200

    init PackageScreen Branch

--- core/src/com/fatman/screen/MainMenuScreen.java
+++ core/src/com/fatman/screen/MainMenuScreen.java
@@ -8,5 +8,191 @@
 
 package com.fatman.screen;
 
-public class MainMenuScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+
+public class MainMenuScreen extends AbstractScreen implements InputProcessor{
+    Sprite m_title;
+
+
+    OrthographicCamera m_camera;
+    private int width = 800;
+    private int height = 480;
+
+    float time = 0;
+
+    public MainMenuScreen(final TheGame game) {
+        super(game);
+        Gdx.input.setInputProcessor(this);
+        m_title = new Sprite(new Texture(Gdx.files.internal("data/sprites/title.png")));
+        m_game.m_batch = new SpriteBatch();
+        m_game.m_batch.getProjectionMatrix().setToOrtho2D(0, 0, 800, 480);
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //delta = Math.min(0.06f, Gdx.graphics.getDeltaTime());
+
+       /* time += delta;
+        if(time < 1.0f){
+            return;
+        }*/
+
+
+        m_game.m_batch.begin();
+
+        m_game.m_batch.draw(m_title, 100, 320, 0, 0, 600, 90f, 1f, 1f, 0);
+
+        m_game.m_batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        this.width = width;
+        this.height = height;
+        if (width == 480 && height == 320) {
+            m_camera = new OrthographicCamera(700, 466);
+            this.width = 700;
+            this.height = 466;
+        } else if (width == 320 && height == 240) {
+            m_camera = new OrthographicCamera(700, 525);
+            this.width = 700;
+            this.height = 525;
+        } else if (width == 400 && height == 240) {
+            m_camera = new OrthographicCamera(800, 480);
+            this.width = 800;
+            this.height = 480;
+        } else if (width == 432 && height == 240) {
+            m_camera = new OrthographicCamera(700, 389);
+            this.width = 700;
+            this.height = 389;
+        } else if (width == 960 && height == 640) {
+            m_camera = new OrthographicCamera(800, 533);
+            this.width = 800;
+            this.height = 533;
+        }  else if (width == 1366 && height == 768) {
+            m_camera = new OrthographicCamera(1280, 720);
+            this.width = 1280;
+            this.height = 720;
+        } else if (width == 1366 && height == 720) {
+            m_camera = new OrthographicCamera(1280, 675);
+            this.width = 1280;
+            this.height = 675;
+        } else if (width == 1536 && height == 1152) {
+            m_camera = new OrthographicCamera(1366, 1024);
+            this.width = 1366;
+            this.height = 1024;
+        } else if (width == 1920 && height == 1152) {
+            m_camera = new OrthographicCamera(1366, 854);
+            this.width = 1366;
+            this.height = 854;
+        } else if (width == 1920 && height == 1200) {
+            m_camera = new OrthographicCamera(1366, 800);
+            this.width = 1280;
+            this.height = 800;
+        } else if (width > 1280) {
+            m_camera = new OrthographicCamera(1280, 768);
+            this.width = 1280;
+            this.height = 768;
+        } else if (width < 800) {
+            m_camera = new OrthographicCamera(800, 480);
+            this.width = 800;
+            this.height = 480;
+        } else {
+            m_camera = new OrthographicCamera(width, height);
+        }
+
+        m_camera.position.x = 400;
+        m_camera.position.y = 240;
+        m_camera.update();
+        m_game.m_batch.getProjectionMatrix().set(m_camera.combined);
+
+    }
+
+    @Override
+    public boolean keyDown(int keycode){
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
+
--- core/src/com/fatman/screen/TheGame.java
+++ core/src/com/fatman/screen/TheGame.java
@@ -1,20 +1,39 @@
 package com.fatman.screen;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.fatman.engine.Level;
+import com.fatman.engine.Player;
 
-public class TheGame extends ApplicationAdapter {
-	//TO-DO
+public class TheGame extends Game {
+	//******************************* ATTRIBUTES ******************************
+	private Level m_level;
+	private Player m_player;
+	public SpriteBatch m_batch;
+	public BitmapFont m_font;
+
+
+	//*******************************   METHODS  ******************************
 	@Override
 	public void create () {
-
+		m_batch = new SpriteBatch();
+		m_font = new BitmapFont();
+		this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
+		super.render();
+	}
 
+	@Override
+	public void dispose(){
+		m_batch.dispose();
+		m_font.dispose();
 	}
 }
