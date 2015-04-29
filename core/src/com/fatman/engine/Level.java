a61204157181ac666ab7bfc8ac53c7b1ce632ef6
Mehdi-Antoine <mehdi.mahfoudi@gmail.com>
2015-04-29 18:09:43 +0200

    Gestion des Levels, belle avancée

--- core/src/com/fatman/engine/Level.java
+++ core/src/com/fatman/engine/Level.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.utils.Array;
 import com.fatman.graphics.Drawable;
 import com.fatman.graphics.Drawer;
 import com.fatman.graphics.LevelDrawer;
+import com.fatman.graphics.LevelModuleDrawer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -34,6 +35,10 @@ public class Level implements Drawable{
 
     //============================= METHODS ==============================//
 
+
+
+    //------ CONSTRUCTEURS
+
     public Level(String pattern_path, ArrayList<LevelModule> levelModules, LevelDrawer levelDrawer){
         m_level_modules = levelModules;
         m_level_scene_patterns = new Array<Pattern>();
@@ -53,9 +58,16 @@ public class Level implements Drawable{
 
     }
 
-    public void storeScenePatterns() throws IOException{
+    //------ CHARGEMENT FICHIERS
 
-        //Lire le fichier scene.info pour savoir le nombre de patterns à enregistrer
+    /*  storeScenePatterns()
+        fonction qui récupère tous les
+        fichiers présents dans le dossier
+        assets/scene_patterns/ et les
+        enregistre dans la variable
+        m_level_scene_patterns
+    */
+    public void storeScenePatterns() throws IOException{
         FileHandle infoFile = Gdx.files.internal(m_pattern_path + "scene_patterns/scene.info");
         BufferedReader info = infoFile.reader(8);
 
@@ -69,16 +81,20 @@ public class Level implements Drawable{
             throw f;
         }
 
-        //Recuperer tous les fichiers et à chaque fois les enregistrer dans l'array
         for(int i = 0; i < patternNb; ++i){
             Pattern p = new Pattern(m_pattern_path + "scene_patterns/" + i + ".pt");
             m_level_scene_patterns.add(p);
         }
-
     }
 
+    /*  storeObjectPatterns()
+        fonction qui récupère tous les
+        fichiers présents dans le dossier
+        assets/object_patterns/ et les
+        enregistre dans la variable
+        m_level_object_patterns
+    */
     public void storeObjectPatterns() throws  IOException{
-        //Lire le fichier scene.info pour savoir le nombre de patterns à enregistrer
         FileHandle infoFile = Gdx.files.internal(m_pattern_path + "object_patterns/object.info");
         BufferedReader info = infoFile.reader(8);
 
@@ -92,23 +108,32 @@ public class Level implements Drawable{
             throw f;
         }
 
-        //Recuperer tous les fichiers et à chaque fois les enregistrer dans l'array
         for(int i = 0; i < patternNb; ++i){
             Pattern p = new Pattern(m_pattern_path + "object_patterns/" + i + ".pt");
             m_level_object_patterns.add(p);
         }
     }
 
+
+    //------ GESTION LEVEL MODULES
+
+    /*  addAtEnd()
+        ajoute un levelModule à la fin
+        de la liste des modules
+    */
     public void addAtEnd(LevelModule levelModule){
         m_level_modules.add(levelModule);
     }
 
+    /*  removeFirst()
+        supprime le premier levelModule
+    */
     public void removeFirst(){
         m_level_modules.remove(0);
     }
 
     public void print(){
-        System.out.println("***** THE_LEVEL *****");
+        System.out.println("********** THE_LEVEL **********");
 
         System.out.println("- pattern_path : " + m_pattern_path);
 
@@ -117,18 +142,25 @@ public class Level implements Drawable{
             l.print();
         }
 
-        System.out.println("- stored scene_patterns : ");
+        System.out.println("**** stored scene_patterns ****");
         for(Pattern p : m_level_scene_patterns){
             p.print();
         }
 
-        System.out.println("- stored object_patterns : ");
+        System.out.println("*** stored object_patterns ****");
         for(Pattern p : m_level_object_patterns){
             p.print();
         }
     }
 
-    public LevelModule genLevelModule(double position){
+    /*  genLevelModule()
+        créée et retourne un levelModule
+        à la position spécifiée
+        les object et scene patterns sont générés
+        aléatoirement
+        le drawer est celui du premier module de la liste
+    */
+    public LevelModule genLevelModule(double position, Drawer levelModuleDrawer){
 
         //trouver aléatoirement un pattern dans l'array de patterns
         int arraySize = m_level_scene_patterns.size;
@@ -139,28 +171,41 @@ public class Level implements Drawable{
         arraySize = m_level_object_patterns.size;
         int randomIndex2 = r.nextInt(arraySize);
 
-        LevelModule levelModule = new LevelModule(m_level_scene_patterns.get(randomIndex), m_level_object_patterns.get(randomIndex2), first().getDrawer());
+        LevelModule levelModule = new LevelModule(m_level_scene_patterns.get(randomIndex), m_level_object_patterns.get(randomIndex2), levelModuleDrawer);
 
         levelModule.setPosition(position);
 
         return levelModule;
     }
 
-    public LevelModule genLevelModule(){
-        return genLevelModule(0);
+    public LevelModule genLevelModule(Drawer levelModuleDrawer){
+        return genLevelModule(0, levelModuleDrawer);
     }
 
+    /*  peek()
+        retourne le dernier level
+        module de la liste
+    */
     public LevelModule peek(){
         return m_level_modules.get(m_level_modules.size() - 1);
     }
 
+    /*  first()
+        retourne le premier level
+        module de la liste
+    */
     public LevelModule first(){
         return m_level_modules.get(0);
     }
 
+    /*  updateList()
+        génére un nouveau module
+        et le place en fin de liste
+        supprime le premier
+    */
     public void updateList(){
         double position = peek().getPosition() + peek().getWidth();
-        LevelModule l = genLevelModule(position);
+        LevelModule l = genLevelModule(position, first().getDrawer());
         addAtEnd(l);
         removeFirst();
         l.notifyChanges();
@@ -170,6 +215,22 @@ public class Level implements Drawable{
         return m_level_modules;
     }
 
+    public LevelModule getModule(int index){
+        return m_level_modules.get(index);
+    }
+
+    public void checkPlayerPosition(double playerPosition){
+        double modulePosition = getModule(0).getPosition() + getModule(0).getWidth();
+        //System.out.println("PlayerPosition : " + playerPosition);
+
+        if(playerPosition > modulePosition){
+
+            System.out.println("Module Changed !");
+            updateList();
+            notifyChanges();
+        }
+    }
+    //------ GESTION DRAWERS
 
     @Override
     public void setDrawer(Drawer d) {
--- core/src/com/fatman/screen/TheGame.java
+++ core/src/com/fatman/screen/TheGame.java
@@ -2,82 +2,89 @@ package com.fatman.screen;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.assets.loaders.BitmapFontLoader;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.fatman.controller.PlayerController;
+import com.fatman.engine.Level;
 import com.fatman.engine.LevelModule;
 import com.fatman.engine.Pattern;
 import com.fatman.engine.Player;
+import com.fatman.graphics.LevelDrawer;
 import com.fatman.graphics.LevelModuleDrawer;
 import com.fatman.graphics.PlayerDrawer;
 import com.fatman.graphics.TileSet;
 
+import java.util.ArrayList;
+
 public class TheGame extends ApplicationAdapter {
 
-	////////////////////////////BATCH
-	private SpriteBatch batch;
+	private SpriteBatch m_batch;
 
-	/////////////////////////////LEVEL
-	private TileSet tileSet;
-	private Texture texture;
-	private LevelModuleDrawer levelModuleDrawer;
-	private Pattern scenePattern;
-	private Pattern objectPattern;
-	private LevelModule levelModule;
-
-	///////////////////////////PLAYER
-	private Player m_player;
-	private PlayerController m_playerController;
-	private PlayerDrawer m_playerDrawer;
-	private TileSet m_tileSetPlayer;
-	private Texture m_texturePlayer;
+	private Level m_level;
+	private LevelDrawer m_level_drawer;
 
 
+	private TileSet m_tile_set;
+	private Texture m_texture;
+
+	private double m_player_position;
+
+	private double m_alpha;
 
 	@Override
 	public void create () {
 
-//		Level level = new Level();
-//		level.print();
+		m_batch = new SpriteBatch();
 
-		//background
-		texture = new Texture(Gdx.files.internal("tileset/latile.png"));
-		tileSet = new TileSet(texture, 64, 64);
-		batch = new SpriteBatch();
-		levelModuleDrawer = new LevelModuleDrawer(tileSet, batch);
-		scenePattern = new Pattern("patterns/scene_patterns/0.pt");
-		objectPattern = new Pattern("patterns/object_patterns/0.pt");
-		levelModule = new LevelModule(scenePattern, objectPattern, levelModuleDrawer);
-		levelModule.notifyChanges();
+		m_texture = new Texture(Gdx.files.internal("tileset/latile.png"));
 
-		//player
-		m_texturePlayer = new Texture(Gdx.files.internal("tileset/larry_run.png"));
-		m_tileSetPlayer = new TileSet(m_texturePlayer, 113, 113);
-		batch = new SpriteBatch();
-		m_playerDrawer = new PlayerDrawer(m_tileSetPlayer, batch);
-		m_player = new Player(m_playerDrawer);
-		m_playerController = new PlayerController(m_player);
-		m_player.notifyChanges();
+		m_tile_set = new TileSet(m_texture, 64, 64);
 
+		m_level_drawer = new LevelDrawer();
+		m_level = new Level("patterns/", new ArrayList<LevelModule>(), m_level_drawer);
+
+		m_level.getLevelModules().add(m_level.genLevelModule(new LevelModuleDrawer(m_tile_set, m_batch)));
+		for(int i = 0; i < 4; ++i){
+			double position = m_level.peek().getPosition() + m_level.peek().getWidth();
+			m_level.addAtEnd(m_level.genLevelModule(position, new LevelModuleDrawer(m_tile_set, m_batch)));
+		}
+
+		m_level.notifyChanges();
+
+		m_level.print();
+
+		m_player_position = 0;
+
+		m_alpha = 0.01;
 
 	}
 
 	@Override
 	public void render () {
-//		batch.begin();
-//			tileSet.getTile(0).draw(batch);
-//			tileSet.getTile(1).setPosition(64, 0);
-//		batch.end();
-//
-//		batch.begin();
-//			tileSet.getTile(1).draw(batch);
-//		batch.end();
 
-		levelModuleDrawer.draw();
-		m_playerDrawer.draw();
 
-		m_playerController.eventHandler();
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		//m_player.increaseX();
+		BitmapFont bitmapFont = new BitmapFont();
+
+		m_batch.begin();
+		m_level_drawer.draw();
+		bitmapFont.draw(m_batch, "PlayerPosition : " + Double.toString(m_player_position), 150, 350);
+		m_batch.end();
+
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+			m_player_position += m_alpha;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+			m_alpha *= 2;
+		}
+
+		m_level.checkPlayerPosition(m_player_position);
+
 	}
 }
