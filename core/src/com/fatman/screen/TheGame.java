50e94e3b88db5a8370b68aeec7394bf43dd75d87
Mehdi-Antoine <mehdi.mahfoudi@gmail.com>
2015-05-01 18:11:34 +0200

    Les tileset de la street !!!!

--- core/src/com/fatman/screen/TheGame.java
+++ core/src/com/fatman/screen/TheGame.java
@@ -61,7 +61,7 @@ public class TheGame extends ApplicationAdapter {
 		m_batch = new SpriteBatch();
 
 		///////////////////////////LEVEL
-		m_tile_set_texture = new Texture(Gdx.files.internal("tileset/latile.png"));
+		m_tile_set_texture = new Texture(Gdx.files.internal("tileset/street_tile.png"));
 		m_game_object_sprite_texture = new Texture(Gdx.files.internal("tileset/food_sprite.png"));
 
 		m_tile_set = new TileSet(m_tile_set_texture, 64, 64);
